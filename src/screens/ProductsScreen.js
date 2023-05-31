50a61f50f6b800d718818e0a53be5f229a1f2618
Sophia Castillo <sphcastillo@gmail.com>
2023-05-31 15:38:27 -0700

    Created src / folders / ProductScreen

---
+++ src/screens/ProductsScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import products from '../data/products';
+
+const ProductsScreen = () => {
+    return (
+        <FlatList 
+            data={products}
+            renderItem={({ item }) => (
+            <View
+            style={styles.imageContainer}
+            >
+                <Image 
+                source={{
+                    uri: item.image,
+                }}
+                style={styles.image}
+                />
+            </View>
+            )}
+            numColumns={2}
+      />
+    )
+}
+
+const styles = StyleSheet.create({
+    imageContainer: {
+      width: '50%',
+      padding: 1
+    },
+    image: {
+      width: "100%",
+      aspectRatio: 1
+    }
+  });
+
+  export default ProductsScreen;
