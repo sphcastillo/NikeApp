96b9c42563215dc47db9d141422826d25a0c777b
Sophia Castillo <sphcastillo@gmail.com>
2023-06-07 09:58:23 -0700

    create apiSlice / added apiSlice to store & added middleware to enable caching, invalidation, polling, etc.

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { productsSlice } from "./productsSlice";
 import { cartSlice } from "./cartSlice";
+import { apiSlice } from "./apiSlice";
 
 export const store = configureStore({
     // take reducers from productsSlice, giving access to the store to these reducers
     reducer: {
         products: productsSlice.reducer,
-        cart: cartSlice.reducer
+        cart: cartSlice.reducer,
+        api: apiSlice.reducer,
     },
+    // Adding the api middleware enables caching, invalidation, polling,
+    // and other useful features of `rtk-query`
+    middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
